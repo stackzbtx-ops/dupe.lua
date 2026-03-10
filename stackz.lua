1202f648376140a0da44c81ffc304a0b77d679f6
stackzbtx-ops <brycedavis829@gmail.com>
2026-03-10 14:08:55 -0400

    Add configuration options to Stackz DUPE HUB

--- stackz.lua
+++ stackz.lua
@@ -4,9 +4,39 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "Stackz DUPE HUB",
+   Icon = 0,
    LoadingTitle = "dupesccript",
    LoadingSubtitle = "by Stackz",
-   Theme = "Default"
+   ShowText = "Rayfield",
+   Theme = "Default",
+
+   ToggleUIKeybind = "K",
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false,
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil,
+      FileName = "stackzdupehub"
+   },
+
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink",
+      RememberJoins = true
+   },
+
+   KeySystem = false,
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = {"Hello"}
+   }
 })
 
 end
